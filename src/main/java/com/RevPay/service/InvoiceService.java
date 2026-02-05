1270a026ce61c9145bb21c61cdabe2b0b3fb56d1
somanadsandeep <116891898+somanadsandeep@users.noreply.github.com>
2026-02-06 01:28:03 +0530

    Updating InvoiceService
    Adding the methods in invoiceService interface

--- src/main/java/com/RevPay/service/InvoiceService.java
+++ src/main/java/com/RevPay/service/InvoiceService.java
@@ -1,7 +1,10 @@
 package com.RevPay.service;
 
 public interface InvoiceService {
-
-	void manageInvoices(int userId) throws Exception;
+void createInvoice(int businessId, String customerEmail, String items, double amount, String paymentTerms) throws Exception;
+    void markInvoicePaid(int invoiceId) throws Exception;
+    void markInvoiceUnpaid(int invoiceId) throws Exception;
+    void viewInvoices(int businessId) throws Exception;
+    void manageInvoices(int businessId) throws Exception;
 
 }
